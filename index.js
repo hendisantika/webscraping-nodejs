63300e58628968b45112af62f1ee674cac6c0590
hendisantika <hendisantika@yahoo.co.id>
2019-06-04 06:28:58 +0700

    #7 - Modify index.js file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const siteUrl = "https://remoteok.io/";
+const cheerio = require("cheerio");
 const axios = require("axios");
+const siteUrl = "https://remoteok.io/";
+let siteName = "";
+const categories = new Set();
+const tags = new Set();
+const locations = new Set();
+const positions = new Set();
+
 const fetchData = async () => {
     const result = await axios.get(siteUrl);
     return cheerio.load(result.data);
